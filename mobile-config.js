069b72d93adc71e52f3948ce1302cbc368cd7fc9
kburts <kevin.burton2@gmail.com>
2015-05-06 23:46:41 -0700

    Added access-control for server in android app.

--- mobile-config.js
+++ mobile-config.js
@@ -24,3 +24,4 @@ App.setPreference('StatusBarStyle', 'default');
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
+App.accessRule('*.kevinsapps.com/*');
