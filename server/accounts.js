44e181ab86a119d16a15533cce84ddd05c6cc09c
kburts <kevin.burton2@gmail.com>
2015-05-21 22:01:43 -0700

    Added some constraints on new username security.

--- server/accounts.js
+++ server/accounts.js
@@ -2,6 +2,28 @@
  * Created by Kevin on 4/27/2015.
  */
 
+Accounts.validateNewUser(function (user) {
+    /*
+        I Don't like putting many constraints on new users signing up but since this is a social site,
+        people should know who exactly they're talking to.
+     */
+    var username = user.username;
+
+    //Make sure the username is of reasonable length.
+    if (username.length <= 3)
+        throw new Meteor.Error('cannot-create-account', 'Username is a little too short, sorry.');
+
+    //Ensure username isn't just user with a space on the end.
+    if (username != username.replace(/(^\s+|\s+$)/g,''))
+        throw new Meteor.Error('cannot-create-account', "Please don't try to impersonate other users.")
+
+    //You username shouldn't just be someone else's with difference case.
+    if (Meteor.users.findOne({'username': username.toLowerCase()}) != null)
+        throw new Meteor.Error('cannot-create-account',
+            'Your username is the same as another but with different case.');
+    return true;
+});
+
 Accounts.onCreateUser(function (options, user) {
     /*
      When a user is created I also want to create a Person to be placed on the map.
